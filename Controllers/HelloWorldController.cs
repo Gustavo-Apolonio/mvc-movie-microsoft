fe0745259424fe649776186f08d6331d1dcce5a6
Gustavo-Apolonio <gustta.apolonio@gmail.com>
2021-09-06 14:56:06 -0300

    Part III - OK

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -1,21 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
         // GET: /HelloWorld/
-        public string Index()
+        public IActionResult Index()
         {
-            return "Esta é minha ação padrão...";
+            return View();
         }
 
         // GET: /HelloWorld/Welcome
         // Requires using System.Text.Encodings.Web;
-        public string Welcome(string name, int ID, int numTimes)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Ola {name}, rodamos {numTimes}x e seu ID eh {ID}");
+            ViewData["Message"] = $"Olá {name}";
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
