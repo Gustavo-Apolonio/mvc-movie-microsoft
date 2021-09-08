36fecee97aeed528be69cf5a26d1853de216533f
Gustavo-Apolonio <gustta.apolonio@gmail.com>
2021-09-08 16:17:08 -0300

    Part IV, V, VI - OK

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
@@ -12,6 +13,8 @@ namespace MvcMovie.Models
         public DateTime ReleaseDate { get; set; }
 
         public string Genre { get; set; }
+
+        [Column(TypeName = "double(18, 2)")]
         public double Price { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Models;
 
 namespace MvcMovie
 {
@@ -13,7 +15,24 @@ namespace MvcMovie
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Um erro occoreu enquanto 'alimentava' o banco de dados.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
