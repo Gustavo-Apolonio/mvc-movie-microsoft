1d9c1da8bfc99002c41e69c3094829d2d4700a0b
Gustavo-Apolonio <gustta.apolonio@gmail.com>
2021-09-09 18:42:24 -0300

    Some reviews

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 namespace MvcMovie.Models
 {
-    public class SeedData
+    public static class SeedData
     {
         public static void Initialize(IServiceProvider serviceProvider)
         {
